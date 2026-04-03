f01a44bf713e4206848c146e05e6df7e23dd5194
Sheheryar Wasti <sheheryar.wasti@gmail.com>
2026-04-02 23:30:26 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        char[] sCharArray = s.toCharArray();
+        char[] tCharArray = t.toCharArray();
+
+        Arrays.sort(sCharArray);
+        Arrays.sort(tCharArray);
+
+        return Arrays.equals(sCharArray, tCharArray);
+
+    }
+}

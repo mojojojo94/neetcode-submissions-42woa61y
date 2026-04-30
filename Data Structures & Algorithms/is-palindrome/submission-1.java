f7b6154d92d71a490c718383306935369b457ae8
Sheheryar Wasti <sheheryar.wasti@gmail.com>
2026-04-30 00:22:57 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() < 1 || s.length() > 1000) {
+            return false;
+        }
+
+        String nS = s.toLowerCase().chars()
+    .filter(Character::isLetterOrDigit)
+    .collect(StringBuilder::new, 
+             StringBuilder::appendCodePoint, 
+             StringBuilder::append)
+    .toString();
+
+        char[] sA = nS.toCharArray();
+        
+        int b = sA.length;
+        
+        for (int a = 0; a < sA.length/2; a++) {
+            if (sA[a] != sA[b-1]) {
+                return false;
+            }
+            b -= 1;
+        }
+     return true; 
+    }
+}

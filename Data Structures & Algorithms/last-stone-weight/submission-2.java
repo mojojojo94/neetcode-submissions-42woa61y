d7d87fc0da64ae0a9b8abbf05f8af8262f4d3c04
Sheheryar Wasti <sheheryar.wasti@gmail.com>
2026-06-22 18:25:47 -0500

    Add: last-stone-weight - submission-2

---
+++ Data Structures & Algorithms/last-stone-weight/submission-2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int heaviest = maxHeap.remove();
+            int secondHeaviest = maxHeap.remove();
+
+            if (heaviest != secondHeaviest) {
+                maxHeap.add(heaviest - secondHeaviest);
+            }
+        }
+
+        return !maxHeap.isEmpty() ? maxHeap.remove() : 0;
+    }
+
+}
